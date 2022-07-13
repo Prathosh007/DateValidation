9f70b6f136660ff8c1edfe3b0563a88024905808
prathosh-14802 <prathosh-14802@csez.zohocorpin.com>
2022-07-13 10:29:55 +0530

    Successfullcm

--- DateValidation.java
+++ DateValidation.java
@@ -1,22 +1,12 @@
 package validateDate;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.Year;
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.Month;
 import java.util.Scanner;
 
 public class DateValidation {
     public static void main(String[] args) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
-
         try {
-
             Scanner in = new Scanner(System.in);
             System.out.print("Enter the date in format [DD/MM/YYYY] :");
             String date = in.nextLine();
@@ -26,12 +16,11 @@ public class DateValidation {
                     System.out.println("Length limit exceeded");
                     System.out.println("Please enter the date in valid format [DD/MM/YYYY]");
                     System.exit(0);
-                } else if (date.charAt(2) != '/' && date.charAt(5) != '/') {
+                } else if (date.charAt(2) != '/' || date.charAt(5) != '/') {
                     System.out.println("Please use '/'");
                     System.out.println("Please enter the date in valid format [DD/MM/YYYY]");
                     System.exit(0);
                 }
-
                 for (int i = 0; i < 2; i++) {
                     if (Character.isDigit(date.toCharArray()[i])) {
                         continue;
@@ -39,7 +28,6 @@ public class DateValidation {
                         System.out.println("Day is not in integer");
                         break;
                     }
-
                 }
                 for (int i = 3; i < 5; i++) {
                     if (Character.isDigit(date.toCharArray()[i])) {
@@ -61,48 +49,43 @@ public class DateValidation {
                 System.out.println("Enter valid date");
             }
 
-            String day = String.valueOf(date.substring(0, 2));
-            String month = String.valueOf(date.substring(3, 5));
-            String year = String.valueOf(date.substring(6, 10));
+            String inputDay = String.valueOf(date.substring(0, 2));
+            String inputMonth = String.valueOf(date.substring(3, 5));
+            String inputYear = String.valueOf(date.substring(6, 10));
 
+            int dd = Integer.parseInt(inputDay);
+            int mm = Integer.parseInt(inputMonth);
+            int yyyy = Integer.parseInt(inputYear);
 
-            int dd = Integer.parseInt(day);
-            int mm = Integer.parseInt(month);
-            int yyyy = Integer.parseInt(year);
-
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
-            Calendar c = Calendar.getInstance();
+            LocalDate givenDate = LocalDate.of(yyyy, mm, dd);
+            System.out.println(date+"---->"+givenDate.getDayOfWeek());
 
             try {
-                YearMonth noOfDays = YearMonth.of(yyyy, mm);
-                int DaysInMonth = noOfDays.lengthOfMonth();
-
                 for (int i = 0; i < 7; i++) {
-                    if (dd < DaysInMonth) {
-                        dd++;
-                    } else if (dd == DaysInMonth && mm != 12) {
-                        dd = 1;
-                        mm++;
-                    } else if (dd == DaysInMonth && mm == 12) {
-                        dd = 1;
-                        mm = 1;
-                        c.set(yyyy, 31, 12);
-                        yyyy++;
-                        c.setTime(dateFormat.parse(date));
-                        c.add(Calendar.DATE, 1);
-                    }
+                    dd = i + 1;
+                    LocalDate nextDate = givenDate.plusDays(dd);
+                    String  currDate= nextDate.toString();
 
-                    LocalDate nextDate = LocalDate.of(yyyy, mm, dd);
+                    String nextDay = String.valueOf(currDate.substring(8, 10));
+                    String nextMonth = String.valueOf(currDate.substring(5, 7));
+                    String nextYear = String.valueOf(currDate.substring(0, 4));
 
-                    System.out.printf(" %d/%d/%d---->", dd, mm, yyyy);
-                    System.out.println(nextDate.getDayOfWeek().toString());
+                    int day = Integer.parseInt(nextDay);
+                    int month = Integer.parseInt(nextMonth);
+                    int year = Integer.parseInt(nextYear);
+
+                    LocalDate finalDate = LocalDate.of(year, month, day);
+
+                    System.out.print(day + "/" + month + "/" + year + "---->");
+                    System.out.println(finalDate.getDayOfWeek());
+
                 }
             } catch (Exception e) {
                 System.out.println("Please enter valid date");
             }
         } catch (Exception e) {
-            System.out.println("Please enter the date in valid format [DD/MM/YYYY]");
+            System.out.println("Please enter valid date");
         }
+
     }
 }
-
